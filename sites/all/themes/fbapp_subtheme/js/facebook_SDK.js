d02d5593d3eff8cb0f16c964cc81839cab7f134f
Michael Harrison <harrison.michael@bitsourceky.com>
2016-06-09 15:55:13 -0400

    working on the js for the app

--- sites/all/themes/fbapp_subtheme/js/facebook_SDK.js
+++ sites/all/themes/fbapp_subtheme/js/facebook_SDK.js
@@ -7,13 +7,14 @@
 		});
 
 	// ADD ADDITIONAL FACEBOOK CODE HERE
+	console.log("hello this is working");
 	};
 
 	(function(d, s, id){
 		var js, fjs = d.getElementsByTagName(s)[0];
 		if (d.getElementById(id)) {return;}
 		js = d.createElement(s); js.id = id;
-		js.src = "//connect.facebook.net/en_US/sdk.js";
+		js.src = "http://connect.facebook.net/en_US/sdk.js";
 		fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
 
--- sites/all/themes/fbapp_subtheme/templates/page--front.tpl.php
+++ sites/all/themes/fbapp_subtheme/templates/page--front.tpl.php
@@ -1,4 +1,4 @@
 <?php 
 	// this is the front page for the app site.
 ?>
-<h1 id="fb-welcome">hello everyone!</h1>
+<h1 id="fb-welcome" class="text-center">hello everyone!</h1>
