0a4b1752457d0c695234013d4560469a03d06c40
Michael Harrison <harrison.michael@bitsourceky.com>
2016-06-10 13:44:27 -0400

    fixed the fb not defined error.

--- sites/all/themes/fbapp_subtheme/js/facebook_SDK.js
+++ sites/all/themes/fbapp_subtheme/js/facebook_SDK.js
@@ -9,6 +9,18 @@
 		});
 
 	// ADD ADDITIONAL FACEBOOK CODE HERE
+		FB.getLoginStatus(function(response) {
+			// Check login status on load, and if the user is
+			// already logged in, go directly to the welcome message.
+			if (response.status == 'connected') {
+				onLogin(response);
+			} else {
+			// Otherwise, show Login dialog first.
+				FB.login(function(response) {
+					onLogin(response);
+				}, {scope: 'user_friends, email'});
+			}
+		});
 		
 		$('.user_clicked').click(function() {
 			console.log("button was clicked.");
@@ -23,8 +35,6 @@
 		fjs.parentNode.insertBefore(js, fjs);
 	}(document, 'script', 'facebook-jssdk'));
 	
-	
-	
 	function onLogin(response) {
 		if (response.status == 'connected') {
 				FB.api('/me?fields=first_name', function(data) {
@@ -33,19 +43,5 @@
 			});
 		}
 	}
-
-	$(document).ready(function(){
-		FB.getLoginStatus(function(response) {
-			// Check login status on load, and if the user is
-			// already logged in, go directly to the welcome message.
-			if (response.status == 'connected') {
-				onLogin(response);
-			} else {
-			// Otherwise, show Login dialog first.
-				FB.login(function(response) {
-					onLogin(response);
-				}, {scope: 'user_friends, email'});
-			}
-		});
-	});
+	
 })(jQuery);
