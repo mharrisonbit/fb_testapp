3d56ff199fe7bfa256d4e505097e4584e605f0cc
Michael Harrison <harrison.michael@bitsourceky.com>
2016-06-10 09:45:11 -0400

    added functionality to the button.

--- sites/all/themes/fbapp_subtheme/templates/page--front.tpl.php
+++ sites/all/themes/fbapp_subtheme/templates/page--front.tpl.php
@@ -2,3 +2,5 @@
 	// this is the front page for the app site.
 ?>
 <h1 id="fb-welcome" class="text-center">hello everyone!</h1>
+	<!-- this is a button that should show me who the user that pressed it is. -->
+<button id="" type="button" class="user_clicked btn btn-primary text-center">click this please</button>
